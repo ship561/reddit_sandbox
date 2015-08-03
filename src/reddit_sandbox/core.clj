0005fc6123c1d4a8d86b0bcaf2b04450a8455abf
shermin <wing561@gmail.com>
2015-08-03 15:59:54 -0400

    Now takes a user name and makes recommendations for subreddits to subcribe to.

--- src/reddit_sandbox/core.clj
+++ src/reddit_sandbox/core.clj
@@ -6,11 +6,6 @@
   (import java.net.URISyntaxException))
 
 ;;;----REDDIT api stuff-------------------------------------------------------------------------
-(defn my-build-comment-url
-  "Probably deprecated"
-  [reddit_id]
-  (str "http://www.reddit.com/user/" reddit_id "/comments.json"))
-
 (defn my-build-pagination
   [& {:keys [after count limit]
       :or {count 0 limit 25}}]
@@ -34,15 +29,32 @@
 
 (defn get-random-author "Get author of random post" [] ((get-random-subreddit-post) :author))
 
+(defn get-author-comments [rc author]
+  (loop [after nil
+         data []
+         sleep? false]
+    (when sleep? (Thread/sleep 2000))
+    (let [xxx (redditclient/urlopen
+               (str (my-url-builder (str "/user/" author "/comments"))
+                    (my-build-pagination :after after :limit 100)) nil)
+          d (concat data (-> xxx :data :children))
+          new-after (-> xxx :data :after)]
+      (if (nil? new-after)
+        d
+        (recur new-after
+               d
+               true)))))
+
 (defn get-author-subreddits
   "Get the subreddits a particular author has commented to"
   [rc author]
-  (when-let [resp (try+ (reddit/user-comments rc author)
+  (when-let [resp (try+ (get-author-comments rc author)
                         (catch [:status 404] {:keys [request-time headers body]};banned
                           (prn :error author body))
+                        (catch [:status 521] {:keys [request-time headers body]} (prn :521 body))
                         (catch URISyntaxException _ (prn :uri));nonstandard character
                         (catch Object _ {:author author} (prn :error author)))];deleted author
-    (map :subreddit resp)))
+    (map #(-> % :data :subreddit) resp)))
 
 ;;;-----------------------------------------------------------------------------------------------
 
@@ -54,7 +66,12 @@
     (/ (count (sets/intersection A B))
       (count (sets/union A B)))))
 
-
+(defn get-neighbors [n usr]
+  (->> (map (partial jaccard-index usr) sample100)
+       (map #(vector (set %1) %2) sample100)
+       (remove #(zero? (second %)))
+       (sort-by second >) ;for k-NN
+       (take n)))
 
 (def sample100
   (let [rc (reddit/login)]
@@ -64,3 +81,14 @@
          (remove nil?)
          (take 100)
          doall)))
+
+(defn make-recommendation [user]
+  (let [usr (get-author-subreddits (reddit/login) user)
+        dists (get-neighbors 10 usr)]
+    (->> dists
+         (map (fn [[subreddits _]] (sets/difference subreddits (set usr))))
+         (apply concat)
+         frequencies ;identify common subreddits
+         (sort-by second >);sort most commmon subreddits among k-NN
+         (map first) ;return top subreddits
+         (take 10))))
